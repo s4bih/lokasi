0a2edc28246c3cce7460604d80b501c2db042d9d
s4bih <msabihamin@gmail.com>
2024-07-11 22:30:22 +0800

    lio

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ def home():
 @app.route('/post')
 def post():
     return render_template('post.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
