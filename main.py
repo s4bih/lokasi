268963a0ba5cdda72b92064c6bc29b7f5392f7d0
s4bih <msabihamin@gmail.com>
2024-07-19 22:28:25 +0800

    lio

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask,render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@127.0.0.1/BlogApp"
+db = SQLAlchemy(app)
 
+class Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    msg = db.Column(db.String(150), nullable=False)
+    date = db.Column(db.String(20), nullable=True)
 @app.route('/')
 def home():
     return render_template('index.html')
